bfd2a4d323d1444e174c3403aee7672f5599e3fe
Mike Limpus <mikelimpus111@gmail.com>
2021-03-21 12:21:03 -0700

    hello

--- CST338_Lab3/src/Deck.java
+++ CST338_Lab3/src/Deck.java
@@ -1,5 +1,8 @@
-// Test
-
+/**
+ * Deck: class object that will contain the source of all Card objects. Contains
+ * and array of Card objects <code>cards[]</cards>. It will contain a multiple 
+ * of 52 cards, as some games rely on multiple decks. 
+ */
 public class Deck {
     public Card dealCard() {
         return null;
